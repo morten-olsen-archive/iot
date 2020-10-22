f4a437843dbab65c68a2e0854c9e2cf044237fd6
Morten Olsen <morten@olsen.pro>
2020-10-22 22:40:35 +0200

    feat: CLI tool

---
+++ packages/cli/src/entries/root.ts
@@ -0,0 +1,12 @@
+import Root from '@morten-olsen/iot-root';
+
+declare var __UNIT_LOCATION: string;
+
+const { default: unit } = require(__UNIT_LOCATION);
+
+const run = async () => {
+  const root = new Root(unit);
+  await root.initialize();
+};
+
+run();
---
+++ packages/demo/src/socket.ts
@@ -0,0 +1,5 @@
+import TestUnit from './TestUnit';
+
+const test = new TestUnit('socket');
+
+export default test;
